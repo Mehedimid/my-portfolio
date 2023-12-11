b484acecf780d619c7dccb090f7e2267c2d12e1d
Mehedimid <mehedihasan25276@gmail.com>
2023-12-11 15:44:10 +0600

    edited to the header

--- src/Pages/home/Header.jsx
+++ src/Pages/home/Header.jsx
@@ -1,7 +1,5 @@
-import me2 from "../../assets/me2.png";
-import me from "../../assets/me.jpg";
-import Lottie from "lottie-react";
-import animation from "../../assets/animation.json"
+import me3 from "../../assets/me3.png";
+import me from "../../assets/me.png";
 
 function Header(props) {
   return (
@@ -10,19 +8,26 @@ function Header(props) {
         <div className="size flex flex-col-reverse lg:flex-row items-center gap-5 justify-center">
           <div className="lg:w-1/2 ">
             <div className="mx-5">
-              <h1 className=" text-3xl text-center md:text-start md:text-5xl font-bold">HI, I'M MEHEDI! CREATIVE DESIGNERCODER</h1>
-              <p className=" py-12 text-center md:text-start">
-              Passionate React developer eager to contribute and learn in a collaborative team environment. Seeking opportunities to apply skills in front-end development, collaborate with experienced professionals, and contribute to innovative projects. Open to mentorship and growth opportunities while aiming to contribute meaningfully to the field.
+              <h1 className=" text-3xl text-center md:text-start md:text-5xl font-bold">
+                HI, I'M MEHEDI! CREATIVE DESIGNERCODER
+              </h1>
+              <p className=" py-8 text-center md:text-start">
+                Passionate React developer eager to contribute and learn in a
+                collaborative team environment. Seeking opportunities to apply
+                skills in front-end development, collaborate with experienced
+                professionals, and contribute to innovative projects. Open to
+                mentorship and growth opportunities while aiming to contribute
+                meaningfully to the field.
               </p>
-              <button className="btn btn-primary">Get Started</button>
+              <button className="common-btn">Get Started</button>
             </div>
           </div>
-
-          <div className="lg:w-1/2 ">
-            <div className="flex justify-center">
-                {/* <img src={me} className="border-b-8 border bg-orange-100 rounded-full border-white w-96 h-[420px] object-cover" /> */}
-                <Lottie animationData={animation} loop={true} />
-                </div>
+          <div className="lg:w-1/2 flex justify-center gradient-shadow">
+            <div className="">
+              <div className=" gradient-box-shadow rounded-full">
+                <img src={me} className="w-full h-full rounded-full bg-orange-300" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -31,3 +36,10 @@ function Header(props) {
 }
 
 export default Header;
+
+{
+  /* <img src={me} className="border-b-8 border bg-orange-100 rounded-full border-white w-96 h-[420px] object-cover" /> */
+}
+{
+  /* <Lottie animationData={animation} loop={true} /> */
+}
